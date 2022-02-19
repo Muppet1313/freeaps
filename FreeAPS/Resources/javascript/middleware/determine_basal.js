92989cbf39c837f43f49d43265861703089d60b6
Jon Mårtensson <jon.m@live.se>
2022-02-19 15:52:15 +0100

    Test calculation for TDD for the last 24 hours

--- FreeAPS/Resources/javascript/middleware/determine_basal.js
+++ FreeAPS/Resources/javascript/middleware/determine_basal.js
@@ -1,5 +1,20 @@
-function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoir, clock) {
-    // modify anything
-    // return any reason what has changed.
-    return "Nothing changed";
+function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoir, clock, pumphistory) {
+     
+    
+    var TDD = 0;
+    var insulin = 0;
+    
+
+    for (let i = 0; i < pumphistory.length; i++) {
+        if (pumphistory[i]._type == "Bolus") {
+            TDD += pumphistory[i].amount;
+        } else if (pumphistory[i]._type == "TempBasal" && pumphistory[i].rate > 0) {
+            insulin = pumphistory[i].rate * (pumphistory[i-1]['duration (min)'] / 60);
+            TDD += insulin;
+        }
+    }
+
+
+    return  "TDD past 24h is: " + TDD.toPrecision(4) + " U";
+
 }
