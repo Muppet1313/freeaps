877dd1b7b1cfe24471f09dffc0e55ae9e4b52944
Jon Mårtensson <jon.m@live.se>
2022-02-20 16:20:36 +0100

    Update TDD middleware. Now running without middleware errors.

--- FreeAPS/Resources/javascript/middleware/determine_basal.js
+++ FreeAPS/Resources/javascript/middleware/determine_basal.js
@@ -1,20 +1,43 @@
 function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoir, clock, pumphistory) {
-     
     
     var TDD = 0;
-    var insulin = 0;
+    var logTDD = "";
+    var current = 0;
     
-
+    // Calculate TDD
+    
+    //Bolus:
     for (let i = 0; i < pumphistory.length; i++) {
         if (pumphistory[i]._type == "Bolus") {
+            // Bolus delivered
             TDD += pumphistory[i].amount;
-        } else if (pumphistory[i]._type == "TempBasal" && pumphistory[i].rate > 0) {
-            insulin = pumphistory[i].rate * (pumphistory[i-1]['duration (min)'] / 60);
-            TDD += insulin;
         }
     }
 
+    // Temp basals:
+    for (let j = 0; j < pumphistory.length; ++j) {
+        if (pumphistory[j]._type == "TempBasal" && pumphistory[j].rate > 0) {
+            current = j;
+            var quota = pumphistory[j].rate;
+            var duration = pumphistory[j-1]['duration (min)'] / 60;
+            var pastTime = new Date(pumphistory[j].timestamp);
+                
+            do {
+                --j;
+            }
+            while (pumphistory[j]._type !== "TempBasal");
+                
+            var morePresentTime = new Date(pumphistory[j].timestamp);
+            var diff = (morePresentTime - pastTime) / 6e4;
+            if (diff > 0 && duration > diff) {
+                duration = diff;
+            }
+            // Calculate temp basals delivered and add to TDD
+            TDD += (quota * duration);
+            j = current;
+        }
+    }
 
-    return  "TDD past 24h is: " + TDD.toPrecision(4) + " U";
-
+    logTDD = "TDD past 24h is: " + TDD.toPrecision(3) + " U";
+    return  logTDD;
 }
