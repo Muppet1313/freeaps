ff8ed5ff8400eee0555e42f6f04886a1f80be633
Jon Mårtensson <jon.m@live.se>
2022-02-20 22:04:14 +0100

    Adding Chris' formula

--- FreeAPS/Resources/javascript/middleware/determine_basal.js
+++ FreeAPS/Resources/javascript/middleware/determine_basal.js
@@ -1,21 +1,44 @@
 function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoir, clock, pumphistory) {
-    
+     
+    // This middleware only works if you have added pumphistory to middleware in FreeAPS X code (my pumphistory branch).
+    const BG = glucose[0].glucose;
+    // Change to false to turn off Chris Wilson's formula
+    var chrisFormula = true;
     var TDD = 0.0;
+    const minLimitChris = profile.autosens_min;
+    const maxLimitChris = profile.autosens_max;
+    const adjustmentFactor = 1;
+    // Your current target, lower limit
+    const currentMinTarget = profile.min_bg;
+    var exerciseSetting = false;
+    var log = "";
     var logTDD = "";
     var current = 0;
+    // If you have not set this to 0.05 in FAX settings (Omnipod), this will be set to 0.1 in code.
     var minimalDose = profile.bolus_increment;
     var insulin = 0.00;
     var incrementsRaw = 0.00;
     var incrementsRounded = 0.00;
     var quota = 0;
     
-    // Calculate TDD
+    if (profile.high_temptarget_raises_sensitivity == true || profile.exercise_mode == true) {
+        exerciseSetting = true;
+    }
     
+    // Turn off Chris' formula (and AutoISF) when using a temp target >= 118 (6.5 mol/l) and if an exercise setting is enabled.
+    // If using AutoISF uncomment the profile.use_autoisf = false
+    if (currentMinTarget >= 118 && exerciseSetting == true) {
+        // profile.use_autoisf = false;
+        chrisFormula = false;
+        log = "Chris' formula off due to a high temp target/exercising. Current min target: " + currentMinTarget;
+    }
+
+    // Calculate TDD --------------------------------------
     //Bolus:
     for (let i = 0; i < pumphistory.length; i++) {
         if (pumphistory[i]._type == "Bolus") {
-            // Bolus delivered
-            TDD += pumphistory[i].amount;
+                // Bolus delivered
+                TDD += pumphistory[i].amount;
         }
     }
 
@@ -30,6 +53,7 @@ function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoi
                 
             do {
                 --j;
+                  
             }
             while (pumphistory[j]._type !== "TempBasal");
                 
@@ -55,7 +79,26 @@ function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoi
             j = current;
         }
     }
+    logTDD = ". TDD past 24h is: " + TDD.toPrecision(3) + " U";
+    // ----------------------------------------------------
+      
+    // Chris' formula:
+    if (chrisFormula == true && TDD > 0) {
+        var newRatio = profile.sens / (277700 / (adjustmentFactor  * TDD * BG));
+        log = "New ratio using Chris' formula is " + newRatio.toPrecision(3) + " with ISF: " + (profile.sens / newRatio).toPrecision(3) + " (" + ((profile.sens / newRatio) * 0.0555).toPrecision(3) + " mmol/l/U)";
 
-    logTDD = "TDD past 24h is: " + TDD.toPrecision(3) + " U";
-    return  logTDD;
+        // Respect autosens.max and autosens.min limits
+        if (newRatio > maxLimitChris) {
+            newRatio = maxLimitChris;
+            log = "Chris' formula hit limit by autosens_max setting: " + maxLimitChris + ". ISF: " + (profile.sens / newRatio).toPrecision(3) + " (" + ((profile.sens / newRatio) * 0.0555).toPrecision(3) + " mmol/l/U)";
+        } else if (newRatio < minLimitChris) {
+            newRatio = minLimitChris;
+            log = "Chris' formula hit limit by autosens_min setting: " + minLimitChris + ". ISF: " + (profile.sens / newRatio).toPrecision(3) + " (" + ((profile.sens / newRatio) * 0.0555).toPrecision(3) + " mmol/l/U)";
+          }
+
+          // Set the new ratio
+            autosens.ratio = newRatio;
+    
+          return log + logTDD;
+    } else { return "Chris' formula is disabled." }
 }
